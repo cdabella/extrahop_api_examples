e7285ac424487209bb477d06c3c6ac61254e50fe
Abella <abella@extrahop.com>
2018-06-11 13:49:55 -0400

    Script cleanup

--- metrics/merge_metric_cycles.py
+++ metrics/merge_metric_cycles.py
@@ -1,28 +1,60 @@
 import requests
 import json
 
+requests.packages.urllib3.disable_warnings()
+
+############################ USER CONFIGURATIONS ###############################
+api_key = ''
+host = ''
+
+use_relative_time = False
+relative_time = -30 * 60 * 1000             # 30min in ms
+absolute_timestamp_start = 1523318400000    # April 10th, 2018 00:00:00
+absolute_timestamp_end = 0                  # Current time
+
+metric_category = "ica_user_detail"
+object_type = "application"
+metric_spec_name = "app_launches"
+########################## END USER CONFIGURATIONS #############################
+
 headers = {'Content-Type': 'application/json',
            'Accept': 'application/json',
-           'Authorization': 'ExtraHop apikey='}
+           'Authorization': 'ExtraHop apikey=' + api_key}
 
-host = ''
+
 url = 'https://' + host + '/api/v1/metrics'
 
-data = {
-    "cycle": "30sec",
-    "from": -1800000,
-    "metric_category": "uri_http_server_detail",
-    "object_type": "device",
-    "metric_specs": [
-        {
-            "name": "req"
-        }
-    ],
-    "object_ids": [
-        1
-    ],
-    "until": 0
-}
+if use_relative_time:
+    data = {
+        "cycle": "auto",
+        "from": relative_time,
+        "metric_category": metric_category,
+        "object_type": object_type,
+        "metric_specs": [
+            {
+                "name": metric_spec_name
+            }
+        ],
+        "object_ids": [
+            0
+        ],
+        "until": 0
+    }
+else:  # Use absolute start time
+    data = {
+        "cycle": "auto",
+        "from": absolute_timestamp_start,
+        "metric_category": metric_category,
+        "object_type": object_type,
+        "metric_specs": [
+            {
+                "name": metric_spec_name
+            }
+        ],
+        "object_ids": [
+            0
+        ],
+        "until": absolute_timestamp_end
 
 rsp = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
 rsp_json = rsp.json()
